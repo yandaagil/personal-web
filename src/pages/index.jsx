fd99c9998817cfdef86e788acacaba3cb1d55dc2
Yanda Agil <yandaagil@gmail.com>
2023-01-22 14:21:15 +0700

    responsive

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Head from "next/head";
 import Footer from "./footer";
+// import SVG from "./components/svg";
 
 export default function Home() {
   return (
@@ -25,8 +26,8 @@ export default function Home() {
           </div>
           <div className="image-wrapper">
             <svg
-              width="400"
-              height="517"
+              // width={props.width}
+              // height={props.height}
               viewBox="0 0 426 543"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
