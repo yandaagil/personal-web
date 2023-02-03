b249764c069b6a3af1b8d28950da4528d846693e
Yanda Agil <yandaagil@gmail.com>
2023-02-03 17:07:53 +0700

    major redesign

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,15 +1,5 @@
 import "@/styles/app.scss";
-// import "../styles/transition.css";
-import Transition from "./components/transition";
-import Navbar from "./components/navbar";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Navbar />
-      <Transition>
-        <Component {...pageProps} />
-      </Transition>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
