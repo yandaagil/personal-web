ead5ea7a8d749d4b37e2914657bd8639aab95f46
Yanda Agil <yandaagil@gmail.com>
2023-10-12 15:11:49 +0700

    change icon

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -6,7 +6,7 @@ const Title = ({ title }: { title: string }) => {
       <title>{title}</title>
       <meta name="description" content="Portfolio" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/y-icon.png" />
     </Head>
   );
 };
