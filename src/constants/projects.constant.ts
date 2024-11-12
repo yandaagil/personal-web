42612e50281430bd11766a99ae7b406a202c53b8
Yanda Agil <yandaagil@gmail.com>
2024-11-12 16:43:43 +0700

    move constants

---
+++ src/constants/projects.constant.ts
@@ -0,0 +1,51 @@
+export type Project = {
+  title: string;
+  repo: string;
+  web?: string;
+};
+
+export const ProjectsList: Project[] = [
+  {
+    title: "to-do",
+    repo: "https://github.com/yandaagil/todo-mern",
+    web: "https://todo-app-yanda.vercel.app/",
+  },
+  {
+    title: "domsat course",
+    repo: "https://github.com/yandaagil/Course",
+    web: "https://domsat-course.vercel.app/",
+  },
+  {
+    title: "moovies",
+    repo: "https://github.com/yandaagil/moovies",
+    web: "https://moovies-yanda.vercel.app/",
+  },
+  {
+    title: "the social",
+    repo: "https://github.com/yandaagil/thesocial",
+  },
+  {
+    title: "inventory management",
+    repo: "https://github.com/yandaagil/inventory-management",
+  },
+  {
+    title: "tic~tac~toe",
+    repo: "https://github.com/yandaagil/tic-tac-toe",
+    web: "https://tic-tac-toe-yanda.vercel.app/",
+  },
+  {
+    title: "infinite scroll",
+    repo: "https://github.com/yandaagil/infinite-scroll",
+    web: "https://infinite-scroll-yanda.vercel.app/",
+  },
+  {
+    title: "star wars",
+    repo: "https://github.com/yandaagil/star-wars",
+    web: "https://star-wars-yanda.vercel.app/",
+  },
+  {
+    title: "bookshelf app",
+    repo: "https://github.com/yandaagil/bookshelf-app",
+    web: "https://bookshelf-app-yanda.vercel.app/",
+  }
+];
---
+++ src/constants/social.constant.ts
@@ -0,0 +1,22 @@
+type SocialMedia = {
+  name: string;
+  link?: string;
+}
+
+export const socialMedia: SocialMedia[] = [
+  {
+    name: "email",
+  },
+  {
+    name: "linkedin",
+    link: "https://www.linkedin.com/in/yandaagil/",
+  },
+  {
+    name: "github",
+    link: "https://www.github.com/yandaagil",
+  },
+  {
+    name: "insta",
+    link: "https://www.instagram.com/yandaagil",
+  },
+]
